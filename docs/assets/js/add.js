0e451c4ee5af0ac1ad6612c42174747b4790ff8b
SanjayTml <sycokto123@gmail.com>
2019-11-09 17:21:50 +0100

    Modify Pay, Dashboard, Register Music

--- docs/assets/js/add.js
+++ docs/assets/js/add.js
@@ -1,38 +1,13 @@
-function addField (argument) {
-        var card-body = document.getElementsByClassName("card-body");
-        var currentIndex = card-body.rows.length;
-        var currentRow = card-body.insertRow(-1);
-
-        var row = document.createElement("div");
-        row.setAttribute("class", "row" + currentIndex);
-		
-		var col = document.createElement("div");
-        col.setAttribute("class", "col-md-12" + currentIndex);
-
-        var formgroup = document.createElement("div");
-        formgroup.setAttribute("class", "form-group" + currentIndex);
-
-        var label = document.createElement("label");
-        label.setAttribute("class", "bmd-labbel-floating" + currentIndex);
-		
-		var textA = document.createElement("input");
-        textA.setAttribute("type", "text", "class", "form-control" + currentIndex);
-
-        var addRowBox = document.createElement("input");
-        addRowBox.setAttribute("type", "button");
-        addRowBox.setAttribute("value", "Add collaborators");
-        addRowBox.setAttribute("onclick", "addField();");
-        addRowBox.setAttribute("class", "button");
-
-        var currentCell = currentRow.insertCell(-1);
-        currentCell.appendChild(linksBox);
-
-        currentCell = currentRow.insertCell(-1);
-        currentCell.appendChild(keywordsBox);
-
-        currentCell = currentRow.insertCell(-1);
-        currentCell.appendChild(violationsBox);
-
-        currentCell = currentRow.insertCell(-1);
-        currentCell.appendChild(addRowBox);
-    }
+$(document).ready(function() {
+    $("#addfield").on("click", function() {  
+                $("#card-collab").append("<div class='collabfield'><div class='row'><div class='col-md-12'>"+
+				"<div class='form-group'><label class='bmd-label-floating'>Collaborator Name</label><input type='text' class='form-control' id='name'>"+
+				"</div></div></div>"+
+				"<div class='row'><div class='col-md-12'>"+
+				"<div class='form-group'><label class='bmd-label-floating'>Collaborator's Ethereum Address</label><input type='text' class='form-control' id='address'>"+
+				"</div></div></div></div>");  
+            });
+	$("#removefield").on("click", function() {  
+                $("#card-collab").children().last().remove();  
+            }); 
+});
